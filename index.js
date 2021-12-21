9db873fbbb15d1abc6cc193440ca1c14dc99ef6c
paapoh <pohjolainenpaavo@gmail.com>
2021-12-22 00:56:09 +0200

    frontend

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('jsonify', (req, res) => { return JSON.stringify(req.body)})
 app.use(morgan((tokens, req, res) => (
