58b91bbfeca5885d6abecd1d3eeeed5578e79c3e
paapoh <pohjolainenpaavo@gmail.com>
2021-12-22 00:00:08 +0200

    heroku fix

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const res = require('express/lib/response')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 morgan.token('jsonify', (req, res) => { return JSON.stringify(req.body)})
@@ -97,7 +99,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
